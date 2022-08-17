737af08e90bdba70a106f9c0c01fd368e295f685
MaratKhabibzhanov <marat.khabibzhanov@mail.ru>
2022-08-17 19:54:54 +0500

    Initial commit

---
+++ mysite/news/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import News
+import re
+from django.core.exceptions import ValidationError
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        #fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control', 'rows': 5
+            }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title.capitalize()
+
+
+    def clean_is_published(self):
+        is_published = self.cleaned_data['is_published']
+        if not is_published:
+            raise ValidationError('При добавлении новости публикация '
+                                  'обязательна!')
+        return is_published
+
+
